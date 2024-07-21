994b001f80986b65b3960b0d74e0d6c7b378c2ff
MrZondi <99998502+MrZondi@users.noreply.github.com>
2024-07-21 16:48:35 +0200

    Add files via upload

---
+++ EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementSystem.Repositories;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+                _employeeRepository = employeeRepository;
+        }
+      
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeesAsync()
+        {
+            return Ok(await _employeeRepository.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEEmpoloyee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new {id = employee.Id}, employee);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> DeleteEmployeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeAsync(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            await _employeeRepository.UpDateEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new {id = employee.Id}, employee);
+        }
+
+    }
+}
