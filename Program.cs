c2e7a73ddd848878a2cea7e489a8d1023c680802
MrZondi <99998502+MrZondi@users.noreply.github.com>
2024-07-21 16:46:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using EmployeeManagementSystem.Data;
+using EmployeeManagementSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace EmployeeManagementSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseInMemoryDatabase("EmployeeDb")
+                );
+
+            builder.Services.AddCors(
+                options =>
+                {
+                    options.AddPolicy("MyCors", builder =>
+                    {
+                        builder.WithOrigins("http://http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+                }
+                );
+            // add employee repository to the DI (dependency injection)
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+           
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
+            app.UseCors("MyCors");
+            app.MapControllers();   
+
+            app.Run();
+        }
+    }
+}
